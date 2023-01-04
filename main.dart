fe1da25da996b06b4b6863cad5d75ff29765ad53
ZERONE <zerone012@gmail.com>
2023-01-04 15:02:50 +0900

    Dart. 함수 관련 공부 중

--- main.dart
+++ main.dart
@@ -3,4 +3,9 @@ void main() {
   var name = 'ZERONE';
   String gender = 'Male';
   final cho = 'ZZZ';
+
+  print(sayHello(name: 'ZERONE', age: 49));
 }
+
+String sayHello({required String name, required int age}) =>
+    "Hi $name, nice to meet you. You are $age years old..";
