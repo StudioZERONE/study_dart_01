86fb2157208384632eb3c92c95201c2ad2d3f1b8
ZERONE <zerone012@gmail.com>
2023-01-05 14:16:19 +0900

    Dart. 초기 학습 완료

--- main.dart
+++ main.dart
@@ -5,7 +5,23 @@ void main() {
   final cho = 'ZZZ';
 
   print(sayHello(name: 'ZERONE', age: 49));
+
+  var xx = Player.createBluePlayer(name: 'a1', age: 19);
 }
 
 String sayHello({required String name, required int age}) =>
     "Hi $name, nice to meet you. You are $age years old..";
+
+class Player {
+  final String name;
+  String team;
+  int age, xp;
+
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.name = name,
+        this.age = age,
+        this.team = 'blue',
+        this.xp = 0;
+}
